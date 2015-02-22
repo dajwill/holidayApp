1e640390337fd2082abfe362bb01db57ee305143
dajwill <will2471@msu.edu>
2015-02-22 18:42:08 -0500

    make it better

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 ruby '2.2.0'
 
 gem 'sinatra', '~> 1.4.5'
+gem 'cat_api'
--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require 'sinatra'
+require 'cat_api'
+
+class CatIndustriesApp < Sinatra::Base
+	get '/cats' do
+		@pictures = CatAPI.new.get_images(category: 'hats')
+		erb :cats
+	end
+end
 
 class MyWebApp < Sinatra::Base
   get '/' do
