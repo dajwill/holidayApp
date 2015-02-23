1e4fad8ea171489046f7d6102ddc426a2f9e4ae2
dajwill <will2471@msu.edu>
2015-02-22 20:17:05 -0500

    make it better

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ require 'holidapi'
 class MyWebApp < Sinatra::Base
   get '/' do
     @holidays = HolidApi.get(country: 'us', year: Time.now.year, month: Time.now.month)
-    @specialHolidays = HolidApi.get(country: 'us', year: '1993', month: '6')
-    
+    @birthHolidays = HolidApi.get(country: 'us', year: '1993', month: '6')
+
     erb :index
   end
 end
