df1855327b94816ff16be56b6d4e01b049598d2a
dajwill <will2471@msu.edu>
2015-02-23 11:49:34 -0500

    adds form info into params array

--- app.rb
+++ app.rb
@@ -3,11 +3,12 @@ require 'holidapi'
 
 class MyWebApp < Sinatra::Base
   get '/' do
+  	puts params
   	erb :index
   end
 
   get '/holidays' do
-    @holidays = HolidApi.get(country: 'us', year: Time.now.year, month: Time.now.month)
+    @holidays = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
     @birthHolidays = HolidApi.get(country: 'us', year: '1993', month: '6')
 
     erb :holidays
