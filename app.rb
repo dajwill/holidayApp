f01fc7586fb3f2bb3048ace7ac76dcee13efae84
dajwill <will2471@msu.edu>
2015-02-23 11:32:19 -0500

    change for index page, adds holidays page

--- app.rb
+++ app.rb
@@ -3,9 +3,13 @@ require 'holidapi'
 
 class MyWebApp < Sinatra::Base
   get '/' do
+  	erb :index
+  end
+
+  get '/holidays' do
     @holidays = HolidApi.get(country: 'us', year: Time.now.year, month: Time.now.month)
     @birthHolidays = HolidApi.get(country: 'us', year: '1993', month: '6')
 
-    erb :index
+    erb :holidays
   end
 end
